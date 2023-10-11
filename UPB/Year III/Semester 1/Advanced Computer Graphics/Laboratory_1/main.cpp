ab1826d55573f8b840492a16bd1eb9d7d950dcc9
Adrian Câmpean <37622282+bimj0rk@users.noreply.github.com>
2023-10-11 20:37:50 +0300

    acg lab 1

--- UPB/Year III/Semester 1/Advanced Computer Graphics/Laboratory_1/main.cpp
+++ UPB/Year III/Semester 1/Advanced Computer Graphics/Laboratory_1/main.cpp
@@ -1,12 +1,6 @@
-// Include standard headers
+#include "Dependencies/GLFW/glfw3.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
 
-// Include GLFW
-#include "dependente/glfw/glfw3.h"
-
-//variables
 GLFWwindow* window;
 const int width = 1024, height = 768;
 
@@ -30,7 +24,19 @@ int main()
 	glfwMakeContextCurrent(window);
 
 	//specify the size of the rendering window
-	#define GL_SILENCE_DEPRECATION
+	glViewport(0, 0, width, height);
+
+	/*
+	For example, a processed point of location (-0.5,0.5) 
+	would be mapped to (200,450) 
+	in screen coordinates if our window is (800, 600). 
+	Note that processed coordinates in OpenGL are between -1 and 1,
+	so we effectively map from the range (-1 to 1) to (0, 800) and (0, 600).
+	Where will (0.25, -0.5) be mapped? 
+	*/
+
+	// Dark blue background
+	glClearColor(0.0, 0.0, 0.4, 0.0);
 	float i = 0.0f;
 	float j = 0.0f;
 
